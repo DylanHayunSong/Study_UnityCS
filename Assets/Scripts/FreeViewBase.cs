8bafd979d589277c3f3f6c06d40bee8a5f36f265
DylanHayunSong <dylanhayunsong@gmail.com>
2020-10-30 15:16:41 +0900

    Change ViemodeManagerUI Add FreeViewBase

---
+++ Assets/Scripts/FreeViewBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeViewBase : ViewModeBase
+{
+    protected override void Initialize ()
+    {
+        isCamLookPivot = false;
+        pivot.transform.position = manager.moveBoundary.transform.position + Vector3.up * 2;
+        remainingPos = Vector3.zero;
+        base.Initialize();
+    }
+    protected override void Move ()
+    {
+        base.Move();
+    }
+    protected override void Rotate ()
+    {
+        base.Rotate();
+    }
+    protected override void Teleport ()
+    {
+        base.Teleport();
+    }
+}
